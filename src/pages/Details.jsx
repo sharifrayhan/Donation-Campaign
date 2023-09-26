33706f3b2594a57fff21c5f990a787ec493c0310
Your Name <you@example.com>
2023-09-26 14:14:08 +0600

    completed details show page design

--- src/pages/Details.jsx
+++ src/pages/Details.jsx
@@ -4,18 +4,31 @@ import { useLoaderData } from "react-router-dom";
 
 const Details = () => {
 
-    const [card, setCard] = useState()
+    const [topic, setTopic] = useState()
     const getIdObject = useParams();
     const {id} = getIdObject
     const cards = useLoaderData()
 
     useEffect(()=>{
         const findCard = cards?.find(card=>card.id==id)
-    },[id,cards])
+        setTopic(findCard)
+        
+    },[id, cards]);
 
+
+    // const {cover_img,card_title,description,card_title_color,price} = topic;
+    // console.log(card_title_color)
+    
     return (
-        <div>
-            
+        <div className='mx-2 md:mx-24 lg:mx-20 my-4'>
+            <div className='relative'>
+            <img className='rounded-lg w-full' src={topic?.cover_img} alt="" />
+            <div className='absolute bottom-0 rounded-b-lg bg-black/80 w-full p-2 md:p-6 lg:p-7 '>
+                <button className="btn btn-xs sm:btn-sm md:btn-sm lg:btn-md text-white" style={{backgroundColor: topic?.card_title_color}}>Donate {topic?.price}</button>
+            </div>
+            </div>
+            <h1 className='font-bold text-xl md:text-3xl lg:text-4xl my-5'>{topic?.card_title}</h1>
+            <p className=' text-[#545454]'>{topic?.description}</p>
         </div>
     );  
 };
